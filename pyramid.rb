cd1a480107f75878be20fd1064bbf8d7d454dc08
Aditya <aanand@grepruby.com>
2017-03-06 15:18:39 +0530

    move unit test cases in test directory

--- pyramid.rb
+++ pyramid.rb
@@ -1,9 +1,9 @@
 class Pyramid
   def star_pyramid(n)
-    pyramid= ''
+    pyramid = ''
     for i in 0..n-1
       for j in 0..(i+n-1)
-        if i+j >=(n-1)
+        if i+j >= (n-1)
           pyramid += "*";
         else
           pyramid += " ";
